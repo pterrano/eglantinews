3c0581f4793d07a0516a31bb6454e3f65cacb536
pterrano <pterrano@gmail.com>
2018-11-10 15:53:34 +0100

    Ajout sécu + constants

---
+++ eglantinews/EglantineConfig.py
@@ -0,0 +1,11 @@
+class EglantineConfig:
+
+    def get_authorised_users(self):
+        return [
+            '...'
+        ]
+
+    def get_authorised_devices(self):
+        return [
+            '...'
+        ]
